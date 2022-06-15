86558f970b21503a28e512e627e17a27d85d691f
filip-andjelic <filip.andjelic.private@gmail.com>
2022-06-15 15:39:58 +0200

    fix: fixed responsive

--- next-docs/pages/templates/sportsbet/vip2.tsx
+++ next-docs/pages/templates/sportsbet/vip2.tsx
@@ -33,8 +33,35 @@ interface Partnership {
   icon: any;
 }
 
+// const VipItem = ({title, icon, text, link}: VipItemProps) => {
+//   return (<div className='w-[272px] h-full bg-gohan rounded-lg flex flex-col p-3 lg:w-[368px] sm:w-[400px] '>
+//       <div className='flex flex-row justify-between items-center mb-8'>
+//         <Image src={Detail} className='rotate-90'/>
+//         <Image src={Detail} className='rotate-180'/>
+//       </div>
+//       <div className='h-4/5 px-12'>
+//         <div className='flex items-center justify-center pt-3 pb-4'>
+//           <Image src={icon} alt={title}/>
+//         </div>
+//         <p className='text-moon-20 text-center mb-4 px-3 w-full'>{title}</p>
+//         <div className='w-full list-disc'>
+//           {text?.map((text: string) => (
+//             <p className="text-moon-14 w-full text-center">{text}</p>
+//           ))}
+//         </div>
+//       </div>
+//       <a
+//         href={link.url}
+//         className='text-moon-14 text-raditz text-center mt-4 -mb-4 px-3 w-full'
+//       >{link.label}</a>
+//       <div className='flex flex-row justify-between items-center mb-0'>
+//         <Image src={Detail}/>
+//         <Image src={Detail} className='rotate-[260deg]'/>
+//       </div>
+//     </div>);
+// };
 const VipItem = ({title, icon, text, link}: VipItemProps) => {
-  return (<div className='w-[272px] h-full bg-gohan rounded-lg flex flex-col p-3 lg:w-[368px]'>
+  return (<div className='w-[330px] h-full bg-[#1A212A] rounded-lg flex flex-col p-2 2xl:w-[390px] xl:w-[335px] lg:w-[400px]'>
       <div className='flex flex-row justify-between items-center mb-8'>
         <Image src={Detail} className='rotate-90'/>
         <Image src={Detail} className='rotate-180'/>
@@ -60,10 +87,9 @@ const VipItem = ({title, icon, text, link}: VipItemProps) => {
       </div>
     </div>);
 };
-
 const PartnershipItems = ({title, icon, text}: Partnership) => {
   return (
-    <div className='w-[272px] bg-gohan rounded-lg flex flex-col p-3 mt-8'>
+    <div className='w-[275px] bg-gohan rounded-lg flex flex-col p-3 mt-8 '>
       <div className='flex flex-row justify-between items-center'>
         <Image src={Detail} className='rotate-90'/>
         <Image src={Detail} className='rotate-180'/>
@@ -236,12 +262,12 @@ const Vip = () => {
 
   return (<div className='flex flex-col sm:px-[10%] sm:py-20 sm:w-full'>
     <div className='max-w-full sm:relative'>
-      <div className='flex flex-col max-w-full px-12 mb-6 z-10000 left-12 bottom-16 sm:w-[500px] sm:absolute'>
-        <p className='mt-6 text-center text-moon-24 text-raditz lg:mt-[70px] sm:text-left'>Username</p>
-        <p className='text-moon-32 text-center p-4 2xl:text-moon-56 md:text-moon-50 sm:text-moon-32 sm:font-semibold sm:text-left sm:p-0 md:mb-[19px]'>
+      <div className='flex flex-col max-w-full px-12 mb-6 z-10000 left-12 bottom-16 sm:w-[500px] sm:mb-0 sm:pb-0 sm:mt-[5%] sm:absolute'>
+        <p className='mt-6 text-center text-moon-24 text-raditz lg:mt-[70px] lg:text-moon-24 sm:text-moon-18  sm:text-left'>Username</p>
+        <p className='text-moon-32 text-center p-4 xl:text-moon-56 lg:text-moon-32 md:text-moon-24 sm:text-moon-16 sm:font-semibold sm:text-left sm:p-0 lg:mb-[17px] sm:mb-[1px]'>
           Take your seat in the VIP club
         </p>
-        <p className='w-full text-moon-18 text-center px-3 pt-2 sm:text-moon-24 sm:text-left sm:p-0'>
+        <p className='w-full text-moon-18 text-center px-3 pt-2 sm:text-moon-14 sm:text-left sm:p-0'>
           Precisely what you imagined your
           <span className='text-raditz'> VIP Experience</span> could be,
           <span className='sm:underline sm:decoration-[#D9BFA4] sm:decoration-1'> only more. </span>
@@ -256,10 +282,11 @@ const Vip = () => {
         </div>
       </div>
     </div>
-    <div className='w-full flex max-w-[100vw] items-center mx-12 py-8 -mt-[60px]'>
-      <div className='w-[900px] overflow-hidden px-1 lg:w-[1150px]'>
+    {/* <div className='w-full flex max-w-[100vw] items-center mx-12 py-8 -mt-[60px]'>
+      <div className='overflow-hidden px-1 lg:w-[1150px] '>
         <Carousel
           selectedIndex={0}
+          hideScrollbar={true}
           items={VipClub.map((item) => (<VipItem
             title={item.title}
             text={item.text}
@@ -268,9 +295,25 @@ const Vip = () => {
           />))}
         />
       </div>
+    </div> */}
+
+
+      <div className='w-full flex flex-col max-w-[100vw] overflow-hidden items-center justify-center py-8 px-6 sm:px-0 sm:py-0  sm:pl-0  '>
+        <Carousel
+          selectedIndex={0}
+          hideScrollbar={true}
+          items={VipClub.map((item) => (<VipItem
+            title={item.title}
+            text={item.text}
+            icon={item.icon}
+            link={item.link}
+          />))}
+        />
+      
     </div>
+
     <div className='mx-12 mt-8'>
-      <div className='flex flex-row justify-center items-center -mb-16 rounded-full h-8 bg-gohan md:w-[30%] sm:w-[35%] '>
+      <div className='flex flex-row justify-center items-center -mb-16 rounded-full h-8 bg-gohan md:w-[34%] sm:w-[50%] '>
         <div className='w-[50%] h-[90%] ml-0.5 flex justify-center items-center rounded-full bg-goku'>
           <p className='text-moon-14 xl:text-moon-14 md:text-moon-12 text-center text-raditz sm:text-moon-10'>
             Past events
@@ -280,9 +323,11 @@ const Vip = () => {
           Upcoming events
         </p>
       </div>
-      <div className='w-[900px] overflow-hidden px-1 lg:w-[1150px]'>
+
+      <div className=' hidden overflow-hidden px-1 lg:w-[1150px] sm:block '>
         <Carousel
           selectedIndex={0}
+          hideScrollbar={true}
           items={Events.map((item) => (<div className='min-w-[900px] mt-24 lg:min-w-[1150px] relative'>
             <div className='w-[70%] h-[50%] min-w-[630px] relative'>
               <Image src={item.heroImage} sizes={'100%'}/>
@@ -314,15 +359,17 @@ const Vip = () => {
           </div>))}
         />
       </div>
+
     </div>
     <div className='w-[100%] sm:hidden'>
       <Image src={PastEvents} sizes={'100%'}/>
     </div>
     <div className='hidden mt-12 sm:block'>
       <p className='text-center pt-4 text-moon-32 font-semibold sm:pl-0'>Partnership</p>
-      <div className='w-[900px] mx-12 lg:w-[1150px]'>
+      <div className=' md:mx-12 xl:w-[100%] lg:w-[100%] sm:mx-0 mr-0'>
         <Carousel
           scrollTo={0}
+          hideScrollbar={true}
           items={Partnership.map((item) => (<PartnershipItems
             title={item.title}
             text={item.text}
@@ -331,7 +378,7 @@ const Vip = () => {
         />
       </div>
       <div className='mt-12 px-12'>
-        <div className='flex flex-row items-center mt-6 mb-4 rounded-full h-8 bg-gohan md:w-[30%] sm:w-[40%] '>
+        <div className='flex flex-row items-center mt-6 mb-4 rounded-full h-8 bg-gohan md:w-[40%] sm:w-[45%] '>
           <div className='w-2/4 h-5/6 ml-0.5 rounded-full bg-goku'>
             <p className='text-moon-14 text-center text-raditz'>Football clubs</p>
           </div>
@@ -340,10 +387,11 @@ const Vip = () => {
       </div>
     </div>
     <p className='text-moon-32 px-5 mt-12 text-center sm:pl-0 sm:px-0'>Here’s what our customers say about being a VIP</p>
-    <div className='w-full flex flex-col justify-center items-center'>
-      <div className='w-[870px] flex flex-row justify-center mx-12'>
+    {/* <div className='w-full flex flex-col justify-center items-center md:w-[80%] md:ml-[10%]'>
+      <div className='md:w-[850px] flex flex-row justify-center mx-12'>
         <Carousel
           scrollTo={0}
+          hideScrollbar={true}
           items={Testimonials.map((item) => (<div className='flex w-[272px] h-[344px] mr-1 mt-20 bg-gohan rounded-lg flex-col p-3'>
             <div className='flex flex-row justify-between items-center'>
               <Image src={Detail} className='rotate-90'/>
@@ -362,8 +410,34 @@ const Vip = () => {
           </div>))}
         />
       </div>
-      <div className='w-[870px] flex flex-col mx-12 sm:flex-row sm:py-20 sm:top-0 sm:items-center'>
-        <div className='flex flex-col max-w-full mt-6 sm:mr-12 sm:w-[600px] sm:justify-center'>
+      </div> */}
+
+      <div className='w-[350] flex flex-col max-w-[100vw] items-center justify-center px-5 py-8 sm:pl-0 xl:pl-[10%] lg:pl-0 md:pl-0'>
+        <Carousel
+          scrollTo={0}
+          hideScrollbar={true}
+          items={Testimonials.map((item) => (
+            <div className='w-[330px] h-full bg-[#1A212A] rounded-lg flex flex-col p-2 xl:w-[345px] lg:w-[330px]' >
+              <div className='flex flex-row justify-between items-center'>
+              <Image src={Detail} className='rotate-90' />
+              <Image src={Detail} className='rotate-180' />
+            </div>
+            <div className='h-4/5 shrink basis-full'>
+                <div className='flex items-center justify-center pt-3 pb-4 px-16 '>
+                  {item.text}
+                </div>
+                <p className='text-moon-16 text-left mb-4 w-full px-16'>{item.name}</p>
+              </div><div className='flex flex-row justify-between items-center '>
+                <Image src={Detail} />
+                <Image src={Detail} className='rotate-[260deg]' />
+
+              </div>
+              </div>))}
+        />
+      </div>
+     
+      <div className=' flex flex-col mx-12 sm:flex-row sm:py-20 sm:top-0 sm:items-center xl:pl-[7%] md:pl-0 sm:pl-[0%]'>
+        <div className='flex flex-col max-w-full mt-6 sm:mr-12 sm:w-[600px] sm:justify-center '>
           <p className='text-center text-moon-24 sm:text-left sm:text-moon-32 sm:font-semibold'>How to become a VIP ?</p>
           <p className='text-moon-16 text-center p-2 sm:text-left sm:pl-0 sm:opacity-80 sm:pt-[12px]'>
             Entry to this exclusive club is by invitation only. The best way
@@ -374,7 +448,7 @@ const Vip = () => {
             Think you have what it takes? Ready to take things to the next level?
             <span className='text-raditz'> Don’t delay</span>
           </p>
-          <Button className='mt-6 mx-5 mb-4 rounded-full h-12 bg-[#49B356] sm:ml-0 sm:w-6/12 lg:w-[55%] md:w-full'>
+          <Button className='mt-6 mx-5 mb-4 rounded-full h-12 bg-[#49B356] xl:w-[37%] lg:w-[55%] md:w-full sm:ml-0 sm:[100%]'>
             Create your account now!
           </Button>
         </div>
@@ -382,7 +456,7 @@ const Vip = () => {
           <Image src={MagicMan}/>
         </div>
       </div>
-    </div>
+    
   </div>);
 }
 
